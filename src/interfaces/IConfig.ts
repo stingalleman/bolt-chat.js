22cd2e22835dd0e51eec50ff9dc66015ee55769e
Sting Alleman <stingalleman@icloud.com>
2021-02-05 09:49:47 +0100

    feat: add pgp support

--- src/interfaces/IConfig.ts
+++ src/interfaces/IConfig.ts
@@ -1,7 +1,9 @@
-import { Identity } from '../lib/User/Identity';
-
 export interface IConfig {
   host: string;
   port?: number;
-  identity: Identity;
+  identity: {
+    username: string;
+    pubKeyFile: string;
+    privKeyFile: string;
+  };
 }
--- src/lib/User/UserManager.ts
+++ src/lib/User/UserManager.ts
@@ -1,3 +1,5 @@
+import * as pgp from 'openpgp';
+import fs from 'fs';
 import { Bolt } from '../Bolt';
 import { Manager } from '../Manager';
 
@@ -5,6 +7,14 @@ import { Manager } from '../Manager';
  * Everything to do with a user.
  */
 export class UserManager extends Manager {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  pubKey: pgp.key.Key;
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  privKey: pgp.key.Key;
+
   constructor(private bolt: Bolt) {
     super('msg');
   }
@@ -17,5 +27,48 @@ export class UserManager extends Manager {
    */
   setUsername(username: string): void {
     this.bolt.config.identity.username = username;
+  }
+
+  /**
+   * Read the public key file.
+   */
+  async readPubKey(): Promise<pgp.key.Key> {
+    const importedPubKey = await pgp.key.readArmored(
+      await fs.promises.readFile(this.bolt.config.identity.pubKeyFile, 'utf-8')
+    );
+
+    [this.pubKey] = importedPubKey.keys;
+    return importedPubKey.keys[0];
+  }
+
+  /**
+   * Read the private key file.
+   */
+  async readPrivKey(): Promise<pgp.key.Key> {
+    const importedPrivKey = await pgp.key.readArmored(
+      await fs.promises.readFile(this.bolt.config.identity.privKeyFile, 'utf-8')
+    );
+
+    [this.privKey] = importedPrivKey.keys;
+    return importedPrivKey.keys[0];
+  }
+
+  /**
+   * Get signing key.
+   *
+   * @param msg Message to sign.
+   */
+  async getSign(msg: string): Promise<string> {
+    if (this.privKey) {
+      const signKey = await pgp.sign({
+        message: pgp.cleartext.fromText(msg),
+        privateKeys: this.privKey,
+        detached: true,
+        armor: true
+      });
+
+      return signKey.signature;
+    }
+    return 'oop';
   }
 }
